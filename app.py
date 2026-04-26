ca52b3a5c2bc3d1dba84399474d9011ecb4cb95d
Sharika Phani <sharikaphani27@gmail.com>
2026-04-27 00:52:20 +0530

    Fix port binding for deployment

--- app.py
+++ app.py
@@ -116,4 +116,5 @@ def health():
 
 if __name__ == '__main__':
     load_model()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
